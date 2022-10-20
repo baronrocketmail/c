31c4febbf4a160694bf8da6845990c538f4a6cbd
sammy freddie <sammyfreddie@sammys-MacBook-Pro.local>
2022-10-20 12:38:52 -0700

    initial99

---
+++ pages/autopay.js
@@ -0,0 +1,10 @@
+import Home from "./index";
+
+export default function Autopay(){
+    return(
+        <div>
+        <Home></Home>
+            <h2>autopay</h2>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,28 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import "@mui/material"
+
+
 
 export default function Home() {
   return (
 
+
+
       <div className={"firstPage"}>
-        <h1>All Payments Up to Date</h1>
+          <Link href="/">
+              <h1>All Payments Up to Date</h1>
+          </Link>
         <div className={"linkBar"}>
-          <h4 className={"itemOne"}>cybernetic stream</h4>
-          <h4 className={"itemTwo"}>autopay</h4>
+            <Link href="/cyberneticstream">
+            <h4 className={"itemOne"}>cybernetic stream</h4>
+            </Link>
+            <Link href="/autopay">
+            <h4 className={"itemTwo"}>autopay</h4>
+            </Link>
+
         </div>
       </div>
   )
